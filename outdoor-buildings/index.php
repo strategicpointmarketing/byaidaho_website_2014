16bf1581c6789a3d77ff7613b70fe5d2a71d93a4
Brian Kelley <Mac@unknown68a86d3a91c0.gateway.2wire.net>
2014-03-17 21:12:10 -0500

    minor fixes

---
+++ outdoor-buildings/index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<!-- Global Meta -->
+	<?php require $_SERVER['DOCUMENT_ROOT'] . '/views/partials/globalMeta.php'; ?>
+	<!-- End Global Meta -->
+	<title>Idaho Outdoor Solutions</title>
+	<meta name="description" content="">
+	<meta name="keywords" content="">
+</head>
+<body>
+	
+	<header>
+		
+		<!-- Main Navigation -->
+		<?php require $_SERVER['DOCUMENT_ROOT'] . '/views/partials/mainNavigation.php'; ?>
+		<!-- End Main Navigation -->
+
+	</header>
+
+	<main role="main">
+		<section class="idaho-outdoor-solutions-outdoor-buildings">
+			<div class="wrapper mtm">
+				<h2 class="primary-heading underline-heading paragon-text mtn">Outdoor Buildings</h2>
+				<ul class="gd-row gt-row unstyled media-grid">
+					<div class="gd-half gd-columns gt-half gt-columns object">
+						<div class="reveal--container">	
+							<img src="http://placehold.it/799x400">													
+						</div>
+						<a class="reveal-content">
+							<span class="reveal-text">Carports</span>
+						</a>	
+					</div>
+					<div class="gd-half gd-columns gt-half gt-columns object">
+						<a href="wooden-sheds.php">
+							<img src="http://placehold.it/799x400">
+						</a>
+					</div>
+				</ul>
+			</div>
+
+		</section>
+	</main>
+
+	<footer>
+
+		<!-- Footer -->
+		<?php require $_SERVER['DOCUMENT_ROOT'] . '/views/partials/footer.php'; ?>
+		<!-- End Footer -->
+
+	</footer>
+
+	<!-- Loading Scripts -->
+	<?php require $_SERVER['DOCUMENT_ROOT'] . '/views/partials/scripts.inc.php'; ?>
+	<!-- End Loading Scripts -->
+
+</body>
+</html>
